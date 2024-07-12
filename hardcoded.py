302326725317daa125610b60f6ca7f6d552ea9bc
SaiChetan338 <saichetankarri9@gmail.com>
2024-07-12 14:40:42 +0530

    h

--- hardcoded.py
+++ hardcoded.py
@@ -2,26 +2,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def quadratic_model(time):
-    a = 0.1
-    b = -1
-    c = 2
+    coefficients = {
+        'a': 0.1,
+        'b': -1,
+        'c': 2
+    }
 
-    temperature = a * (time ** 2) + b * time + c
-    return temperature
+    temperature = coefficients['a'] * (time ** 2) + coefficients['b'] * time + coefficients['c']
+    return temperature, coefficients
 
 def main():
     time_values = np.linspace(0, 10, 50)
-    temperature_values = quadratic_model(time_values)
+    temperature_values, coefficients = quadratic_model(time_values)
     
-    a = 0.1
-    b = -1
-    c = 2
-    
-    plt.plot(time_values, temperature_values, label=f'Hard Coded a={a}, b={b}, c={c}')
+    plt.plot(time_values, temperature_values, label=f'Hard Coded a={coefficients["a"]}, b={coefficients["b"]}, c={coefficients["c"]}')
     plt.xlabel('Time')
     plt.ylabel('Temperature')
-    plt.title('Hard Coded')
+    plt.title('Quadratic Model')
     plt.legend()
+    plt.grid()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
     plt.show()
 
 if __name__ == '__main__':
