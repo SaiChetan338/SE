aedfaabb4b354fe5e92021ab74a62c9770bd6662
SaiChetan338 <saichetankarri9@gmail.com>
2024-07-12 14:13:10 +0530

    Hardcoded input

--- hardcoded.py
+++ hardcoded.py
@@ -6,17 +6,21 @@ def quadratic_model(time):
     b = -1
     c = 2
 
-    temperature = a*(time ** 2) + b * time + c
+    temperature = a * (time ** 2) + b * time + c
     return temperature
 
 def main():
     time_values = np.linspace(0, 10, 50)
     temperature_values = quadratic_model(time_values)
-
-    plt.plot(time_values, temperature_values, label='Hard-Coded Coefficient')
+    
+    a = 0.1
+    b = -1
+    c = 2
+    
+    plt.plot(time_values, temperature_values, label=f'Hard Coded a={a}, b={b}, c={c}')
     plt.xlabel('Time')
     plt.ylabel('Temperature')
-    plt.title('Quadratic Model')
+    plt.title('Hard Coded')
     plt.legend()
     plt.show()
 
